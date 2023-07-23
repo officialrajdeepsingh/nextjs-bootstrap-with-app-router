35947d9398d5ec4d2d07b488ed64c754be8e6111
officialrajdeepsingh <radewala.2@gmail.com>
2023-07-23 18:44:39 +0530

    update the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-<>
-        <div className="container my-4">
+    <>
+      <div className="container my-4">
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">Special title treatment</h5>
@@ -12,7 +11,7 @@ export default function Home() {
               With supporting text below as a natural lead-in to additional
               content.
             </p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <Link href="#" className="btn btn-primary">Go somewhere</Link>
           </div>
         </div>
       </div>
@@ -28,7 +27,9 @@ export default function Home() {
                   lead-in to additional content. This content is a little bit
                   longer.
                 </p>
-                <button type="button" className="btn btn-primary">Read Now</button>
+                <button type="button" className="btn btn-primary">
+                  Read Now
+                </button>
               </div>
             </div>
           </div>
@@ -42,7 +43,9 @@ export default function Home() {
                   lead-in to additional content. This content is a little bit
                   longer.
                 </p>
-                <button type="button" className="btn btn-primary">Read Now</button>
+                <button type="button" className="btn btn-primary">
+                  Read Now
+                </button>
               </div>
             </div>
           </div>
@@ -55,14 +58,15 @@ export default function Home() {
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content.
                 </p>
-                <button type="button" className="btn btn-primary">Read Now</button>
+                <button type="button" className="btn btn-primary">
+                  Read Now
+                </button>
               </div>
             </div>
           </div>
 
           <div className="col">
             <div className="card">
-
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">
@@ -70,7 +74,9 @@ export default function Home() {
                   lead-in to additional content. This content is a little bit
                   longer.
                 </p>
-                <button type="button" className="btn btn-primary">Read Now</button>
+                <button type="button" className="btn btn-primary">
+                  Read Now
+                </button>
               </div>
             </div>
           </div>
@@ -78,23 +84,38 @@ export default function Home() {
       </div>
 
       <div className="container my-4">
-
-        <button className="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-          
+        <button
+          className="btn btn-primary"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapseExample"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+        >
           collapse
-        
         </button>
 
         <div className="collapse" id="collapseExample">
-          
           <div className="card card-body">
-            Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+            Some placeholder content for the collapse component. This panel is
+            hidden by default but revealed when the user activates the relevant
+            trigger.
           </div>
-
         </div>
+      </div>
 
+      <div className="container my-5">
+        <button
+          type="button"
+          className="btn btn-secondary"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          data-bs-custom-class="custom-tooltip"
+          data-bs-title="This top tooltip is themed via CSS variables."
+        >
+          Custom tooltip
+        </button>
       </div>
     </>
-
-  )
+  );
 }
